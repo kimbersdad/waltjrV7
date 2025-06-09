1dc0500134485e14e80171ce211c4956bf9148e4
kimbersdad <126426335+kimbersdad@users.noreply.github.com>
2025-06-09 08:53:03 -0400

    Update quote.js

--- public/quote.js
+++ public/quote.js
@@ -24,4 +24,25 @@ window.addEventListener("load", () => {
       .catch(showLogin);
 
     Outseta.on("accessToken.set", showChat);
-    Outseta.o
+    Outseta.on("accessToken.removed", showLogin);
+  } else {
+    console.warn("⚠️ Outseta not ready");
+    showLogin();
+  }
+
+  loginBtn?.addEventListener("click", () => {
+    if (window.Outseta?.toggleLogin) {
+      Outseta.toggleLogin();
+    } else {
+      window.location.href =
+        "https://waltjr.outseta.com/auth?widgetMode=login&redirectUrl=https://waltjr.netlify.app/quote.html";
+    }
+  });
+
+  logoutBtn?.addEventListener("click", () => {
+    Outseta.logout().then(() => {
+      window.location.href =
+        "https://waltjr.outseta.com/auth?widgetMode=login&redirectUrl=https://waltjr.netlify.app/quote.html";
+    });
+  });
+});
